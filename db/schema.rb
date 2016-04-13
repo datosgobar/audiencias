ae8f4bd4341b1903746e34ddd71a416acd7dce68
Ignacio Heredia <ignacio.nh@gmail.com>
2016-04-13 18:32:21 -0300

    Fix - error de nombre en archivo de migracion

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 201604012174200) do
+ActiveRecord::Schema.define(version: 20160412174200) do
 
   create_table "admin_associations", force: :cascade do |t|
     t.integer  "user_id",       null: false
