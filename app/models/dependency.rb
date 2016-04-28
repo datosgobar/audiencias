a6c5729aa33df3ba408bab32ddb4287d2e22d79e
Ignacio Heredia <ignacio.nh@gmail.com>
2016-04-28 13:07:32 -0300

    Maquetado inicial para arbol de dependencias

--- app/models/dependency.rb
+++ app/models/dependency.rb
@@ -10,7 +10,21 @@ class Dependency < ActiveRecord::Base
 	validates :name, length: { minimum: 6 }
 
   def self.tree 
-    active_dependencies = where(active: true)
+    active_dependencies = where(active: true).as_json
+    
+    active_dependencies.each do |dependency|
+      if dependency['parent_id']
+        parent = active_dependencies.find { |parent| parent['id'] == dependency['parent_id'] }
+        if parent 
+          unless parent['children']
+            parent['children'] = []
+          end
+          parent['children'] << dependency
+        end
+      end
+    end
+
+    active_dependencies.select { |d| !d['parent_id'] }
   end
 
 end
